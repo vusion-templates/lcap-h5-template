e587b19bc6ebadd992a2a90174122a876b85c6cf
Bener <benjavan@163.com>
2022-06-21 17:23:17 +0800

    feat: extra builtin function

--- src/global/features/utils/install.js
+++ src/global/features/utils/install.js
@@ -1,54 +1,51 @@
-import cloneDeep from 'lodash/cloneDeep';
-import isObject from 'lodash/isObject';
-import isEqual from 'lodash/isEqual';
-import { utils as cutils } from 'cloud-ui.vusion';
-import { addDays, subDays, addMonths, format, formatRFC3339, isValid } from 'date-fns';
+import cloneDeep from "lodash/cloneDeep";
+import isObject from "lodash/isObject";
+import isEqual from "lodash/isEqual";
+import { utils as cutils } from "cloud-ui.vusion";
+import {
+    addDays,
+    subDays,
+    addMonths,
+    format,
+    formatRFC3339,
+    isValid,
+} from "date-fns";
 let enumsMap = {};
 
 function toValue(date, converter) {
-    if (!date)
-        return date;
-    if (converter === 'format')
-        return this.format(date, 'YYYY-MM-DD'); // value 的真实格式
-    else if (converter === 'json')
-        return date.toJSON();
-    else if (converter === 'timestamp')
-        return date.getTime();
-    else
-        return date;
+    if (!date) return date;
+    if (converter === "format")
+        return this.format(date, "YYYY-MM-DD"); // value 的真实格式
+    else if (converter === "json") return date.toJSON();
+    else if (converter === "timestamp") return date.getTime();
+    else return date;
 }
 
 export const utils = {
     Vue: undefined,
     Enum(enumName, value) {
-        if (arguments.length === 0)
-            return '';
-        else if (arguments.length === 1)
-            return enumsMap[enumName];
-        else if (enumsMap[enumName])
-            return enumsMap[enumName](value);
-        else
-            return '';
+        if (arguments.length === 0) return "";
+        else if (arguments.length === 1) return enumsMap[enumName];
+        else if (enumsMap[enumName]) return enumsMap[enumName](value);
+        else return "";
     },
     EnumValue(enumName, value) {
         return value;
     },
     EnumLabel(enumName, value) {
-        if (arguments.length === 0)
-            return '';
-        else if (arguments.length === 1)
-            return enumsMap[enumName];
-        else if (enumsMap[enumName])
-            return enumsMap[enumName](value);
-        else
-            return '';
+        if (arguments.length === 0) return "";
+        else if (arguments.length === 1) return enumsMap[enumName];
+        else if (enumsMap[enumName]) return enumsMap[enumName](value);
+        else return "";
     },
     EnumList(enumName, value) {
         const enumeration = enumsMap[enumName];
-        if (!enumeration)
-            return [];
+        if (!enumeration) return [];
         else
-            return Object.keys(enumeration).map((key) => ({ text: enumeration[key], value: key }));
+            return Object.keys(enumeration).map((key) => ({
+                text: enumeration[key],
+                value: key,
+            }));
     },
     Split(str, seperator) {
         return str && str.split(seperator);
@@ -61,8 +58,7 @@ export const utils = {
     Concat(str1, str2) {
         if (Array.isArray(str1) && Array.isArray(str2))
             return [].concat(str1, str2);
-        else
-            return str1 + str2;
+        else return str1 + str2;
     },
     Length(str1) {
         return str1 && str1.length;
@@ -85,7 +81,7 @@ export const utils = {
         return utils.Vue.set(arr, index, item);
     },
     Contains(arr, item) {
-        return typeof arr.find((ele) => isEqual(ele, item)) !== 'undefined';
+        return typeof arr.find((ele) => isEqual(ele, item)) !== "undefined";
     },
     Add(arr, item) {
         if (Array.isArray(arr)) {
@@ -109,32 +105,30 @@ export const utils = {
         }
     },
     CurrDate() {
-        return new Date().toJSON().replace(/T.+?Z/, '');
+        return new Date().toJSON().replace(/T.+?Z/, "");
     },
     CurrTime() {
-        return new Date().toTimeString().split(' ')[0];
+        return new Date().toTimeString().split(" ")[0];
     },
     CurrDateTime() {
         return new Date().toJSON();
     },
-    AddDays(date = new Date(), amount = 1, converter = 'json') {
+    AddDays(date = new Date(), amount = 1, converter = "json") {
         return toValue(addDays(new Date(date), amount), converter);
     },
-    AddMonths(date = new Date(), amount = 1, converter = 'json') {
+    AddMonths(date = new Date(), amount = 1, converter = "json") {
         /** 传入的值为标准的时间格式 */
         return toValue(addMonths(new Date(date), amount), converter);
     },
-    SubDays(date = new Date(), amount = 1, converter = 'json') {
+    SubDays(date = new Date(), amount = 1, converter = "json") {
         return toValue(subDays(new Date(date), amount), converter);
     },
     FormatDate(value, formatter) {
-        if (!value)
-            return '-';
+        if (!value) return "-";
         return cutils.dateFormatter.format(value, formatter);
     },
     FormatDateTime(value, formatter) {
-        if (!value)
-            return '-';
+        if (!value) return "-";
         return cutils.dateFormatter.format(value, formatter);
     },
     Clone(obj) {
@@ -151,8 +145,7 @@ export const utils = {
             obj.length = 0;
         } else if (isObject(obj)) {
             for (const key in obj) {
-                if (obj.hasOwnProperty(key))
-                    obj[key] = undefined;
+                if (obj.hasOwnProperty(key)) obj[key] = undefined;
             }
         } else {
             obj = undefined;
@@ -164,8 +157,7 @@ export const utils = {
      */
     ClearObject(obj) {
         for (const key in obj) {
-            if (obj.hasOwnProperty(key))
-                obj[key] = undefined;
+            if (obj.hasOwnProperty(key)) obj[key] = undefined;
         }
         return obj;
     },
@@ -178,11 +170,11 @@ export const utils = {
             min = 0;
         }
 
-        if (typeof min !== 'number' || typeof max !== 'number') {
-            throw new TypeError('Expected all arguments to be numbers');
+        if (typeof min !== "number" || typeof max !== "number") {
+            throw new TypeError("Expected all arguments to be numbers");
         }
 
-        return Math.floor((Math.random() * (max - min + 1)) + min);
+        return Math.floor(Math.random() * (max - min + 1) + min);
     },
     tryJSONParse(str) {
         let result;
@@ -196,99 +188,172 @@ export const utils = {
     Convert(value, schema) {
         const { type, format: formatVar } = schema;
 
-        if (type === 'string') {
+        if (type === "string") {
             switch (formatVar) {
-                case 'date-time':
+                case "date-time":
                     return formatRFC3339(new Date(value));
-                case 'date':
-                    return format(new Date(value), 'yyyy-MM-dd');
-                case 'time':
-                    return format(new Date(value), 'HH:mm:ss');
-                case '': // 字符串
+                case "date":
+                    return format(new Date(value), "yyyy-MM-dd");
+                case "time":
+                    return format(new Date(value), "HH:mm:ss");
+                case "": // 字符串
                     return String(value);
             }
         }
 
-        if (type === 'number' && formatVar === 'double') // 小数
+        if (type === "number" && formatVar === "double")
+            // 小数
             return parseFloat(+value);
 
-        if (type === 'integer')
+        if (type === "integer")
             // 日期时间格式特殊处理; 整数： format 'int' ; 长整数: format: 'long'
-            return /^\d{4}-\d{2}-\d{2}(.*)+/.test(value) ? new Date(value).getTime() : Math.round(+value);
+            return /^\d{4}-\d{2}-\d{2}(.*)+/.test(value)
+                ? new Date(value).getTime()
+                : Math.round(+value);
 
-        if (type === 'boolean') // 布尔值
+        if (type === "boolean")
+            // 布尔值
             return !!value;
     },
     /**
      * 数字格式化
      * @param {digits} 小数点保留个数
      * @param {showGroup} 是否显示千位分割（默认逗号分隔）
-    */
+     */
     FormatNumber(value, digits, showGroup) {
-        if (!value)
-            return value;
-        if (parseFloat(value) === 0)
-            return '0';
-        if (isNaN(parseFloat(value)) || isNaN(parseInt(digits)))
-            return;
+        if (!value) return value;
+        if (parseFloat(value) === 0) return "0";
+        if (isNaN(parseFloat(value)) || isNaN(parseInt(digits))) return;
         if (digits !== undefined) {
             value = Number(value).toFixed(parseInt(digits));
         }
         if (showGroup) {
-            const temp = ('' + value).split('.');
+            const temp = ("" + value).split(".");
             const right = temp[1];
-            let left = temp[0].split('').reverse().join('').match(/(\d{1,3})/g).join(',').split('').reverse().join('');
-            if (temp[0][0] === '-')
-                left = '-' + left;
-            if (right)
-                left = left + '.' + right;
+            let left = temp[0]
+                .split("")
+                .reverse()
+                .join("")
+                .match(/(\d{1,3})/g)
+                .join(",")
+                .split("")
+                .reverse()
+                .join("");
+            if (temp[0][0] === "-") left = "-" + left;
+            if (right) left = left + "." + right;
             value = left;
         }
-        return '' + value;
+        return "" + value;
     },
     /**
      * 时间差
      * @param {dateTime1} 时间
      * @param {dateTime2} 时间
      * @param {calcType} 计算类型：天数(day)、小时数(hour)、分钟数(minute)、秒数(second)
-    */
+     */
     DateDiff(dateTime1, dateTime2, calcType) {
-        if (!dateTime1 || !dateTime2)
-            return;
+        if (!dateTime1 || !dateTime2) return;
         if (!isValid(new Date(dateTime1)) || !isValid(new Date(dateTime2)))
             return;
         const map = {
             d: {
                 diff: 24 * 60 * 60 * 1000,
-                formatter: 'yyyy-MM-dd',
+                formatter: "yyyy-MM-dd",
             },
             h: {
                 diff: 60 * 60 * 1000,
-                formatter: 'yyyy-MM-dd HH:mm',
+                formatter: "yyyy-MM-dd HH:mm",
             },
             m: {
                 diff: 60 * 1000,
-                formatter: 'yyyy-MM-dd HH:mm',
+                formatter: "yyyy-MM-dd HH:mm",
             },
             s: {
                 diff: 1000,
-                formatter: 'yyyy-MM-dd HH:mm:ss',
+                formatter: "yyyy-MM-dd HH:mm:ss",
             },
         };
-        if (!map[calcType])
-            return;
+        if (!map[calcType]) return;
         const config = map[calcType];
-        const dateTime1Temp = new Date(format(new Date(dateTime1), config.formatter)).getTime();
-        const dateTime2Temp = new Date(format(new Date(dateTime2), config.formatter)).getTime();
+        const dateTime1Temp = new Date(
+            format(new Date(dateTime1), config.formatter)
+        ).getTime();
+        const dateTime2Temp = new Date(
+            format(new Date(dateTime2), config.formatter)
+        ).getTime();
         const dateDiff = dateTime2Temp - dateTime1Temp;
-        return Math.floor(dateDiff / (config.diff));
+        return Math.floor(dateDiff / config.diff);
     },
     GetProperties(name) {
         let customerProperties = window.__LCAP_CONFIG__;
-        if (typeof customerProperties === 'string') {
+        if (typeof customerProperties === "string") {
             customerProperties = JSON.parse(customerProperties);
         }
         return customerProperties[name];
+    },
+    /**
+     * 字符串查找
+     * @param {string} str 字符串
+     * @param {string} search 查找字符串
+     * @param {number} fromIndex 开始位置
+     * @param {boolean} ignoreCase 是否忽略大小写
+     * @returns {number} 查找到的位置，没找到返回-1
+     */
+    IndexOf(str, search, fromIndex, ignoreCase) {
+        if (typeof str !== "string" || typeof search !== "string") {
+            return -1;
+        }
+        if (fromIndex === undefined) {
+            fromIndex = 0;
+        }
+        if (ignoreCase) {
+            str = str.toLowerCase();
+            search = search.toLowerCase();
+        }
+        return str.indexOf(search, fromIndex);
+    },
+    /**
+     * 倒序字符串查找
+     * @param {string} str 字符串
+     * @param {string} search 查找字符串
+     * @param {boolean} ignoreCase 是否忽略大小写
+     * @returns {number} 查找到的位置，没找到返回-1
+     */
+    LastIndexOf(str, search, ignoreCase) {
+        if (typeof str !== "string" || typeof search !== "string") {
+            return -1;
+        }
+        if (ignoreCase) {
+            str = str.toLowerCase();
+            search = search.toLowerCase();
+        }
+        return str.lastIndexOf(search);
+    },
+    /**
+     *
+     * @param {string} str 字符串
+     * @param {string} search 查找字符串
+     * @param {string} replace 替换字符串
+     * @returns {string} 替换后的字符串
+     */
+    Replace(str, search, replace) {
+        if (typeof str !== "string" || typeof search !== "string") {
+            return str;
+        }
+        return str.replace(search, replace);
+    },
+    /**
+     *
+     * @param {string} str 字符串
+     * @param {number} start 开始位置
+     * @param {number} length 长度
+     * @returns {string} 截取后的字符串
+     */
+    SubString(str, start, length) {
+        if (typeof str !== "string") {
+            return str;
+        }
+        return str.substr(start, length);
     },
 };
 
