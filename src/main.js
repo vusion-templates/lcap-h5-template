0f19616de2d1be0fb1d5d3a4d7339f3bdfc3248f
hzlingqiao <hzlingqiao@corp.netease.com>
2022-10-21 12:33:13 +0800

    chore: 调整组建库插件的注册位置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import Vue from 'vue';
-import { installOptions, installDirectives, installComponents } from '@vusion/utils';
-import * as Vant from '@lcap/mobile-ui/dist-theme';
-import * as CloudUI from 'cloud-ui.vusion/dist';
-
 import metaData from './metaData.json';
 import platformConfig from './platform.config.json';
 import { routes } from './router/routes';
@@ -10,10 +5,3 @@ import './library';
 import * as cloudAdminDesigner from './init';
 
 cloudAdminDesigner.init(platformConfig?.appConfig, platformConfig, routes, metaData);
-
-installOptions(Vue);
-installDirectives(Vue, CloudUI.directives);
-installComponents(Vue, CloudUI);
-Vue.mixin(CloudUI.MEmitter);
-Vue.mixin(CloudUI.MPubSub);
-Vue.use(Vant);
