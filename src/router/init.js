c598bb01858ff62f2dc286f0d911efdbcfda779b
梦不觉 <huangjianqiang126@163.com>
2022-11-24 15:36:41 +0800

    feat: ignore guard error

--- src/router/init.js
+++ src/router/init.js
@@ -1,6 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+const originalPush = VueRouter.prototype.push;
+
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+    if (onResolve || onReject)
+        return originalPush.call(this, location, onResolve, onReject);
+    return originalPush.call(this, location).catch((err) => {
+        if (VueRouter.isNavigationFailure(err)) {
+        // resolve err
+            return err;
+        }
+        // rethrow error
+        return Promise.reject(err);
+    });
+};
+
 export function initRouter(routes) {
     Vue.use(VueRouter);
 
