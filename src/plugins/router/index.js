de06453afd7e54fe7be0c86df606d30813aa82d4
hzlifuquan <hzlifuquan@corp.netease.com>
2022-12-05 15:19:51 +0800

    fix: 控制台报错，NavigationDuplicated: Avoided redundant navigation to current location

--- src/plugins/router/index.js
+++ src/plugins/router/index.js
@@ -14,11 +14,10 @@ export default {
             if (url.startsWith('http'))
                 location.href = encodeUrl(url);
             else {
-                try {
-                    this.$router.push(url);
-                } catch(err) {
-                    console.log(err);
-                }
+                this.$router.push(url)
+                    .catch((err) => {
+                        console.log(err);
+                    });
             }
         };
     },
