3c2e539d9ce887a8af7dd25a17e10420fcb87025
jiyufeng <18012261618@126.com>
2023-02-20 17:23:45 +0800

    fix: 错误提示位置

--- src/utils/create/errHandles.js
+++ src/utils/create/errHandles.js
@@ -4,35 +4,53 @@ import { VanToast as Toast } from '@lcap/mobile-ui';
 export default {
     defaults({ config }, err) {
         if (!config.noErrorTip) {
-            Toast(err.msg || '系统错误，请联系管理员！');
+            Toast({
+                message: err.msg || '系统错误，请联系管理员！',
+                position: 'top',
+            });
         }
     },
     500({ config }, err = {}) {
         if (!config.noErrorTip) {
-            Toast(err.msg || '系统错误，请联系管理员！');
+            Toast({
+                message: err.msg || '系统错误，请联系管理员！',
+                position: 'top',
+            });
         }
     },
     400({ config }, err = {}) {
         if (!config.noErrorTip) {
-            Toast(err.msg || '系统错误，请联系管理员！');
+            Toast({
+                message: err.msg || '系统错误，请联系管理员！',
+                position: 'top',
+            });
         }
     },
     403({ config }, err = {}) {
         if (err.Code === 'InvalidToken' && err.Message === 'Token is invalid') {
             if (!config.noErrorTip) {
-                Toast('登录失效', '请重新登录');
+                Toast({
+                    message: '登录失效，请重新登录',
+                    position: 'top',
+                });
             }
             location.href = '/login';
         }
     },
     remoteError({ config }, err) {
         if (!config.noErrorTip) {
-            Toast('系统错误，请联系管理员！');
+            Toast({
+                message: '系统错误，请联系管理员！',
+                position: 'top',
+            });
         }
     },
     localError({ config }, err) {
         if (!config.noErrorTip) {
-            Toast('系统错误，请联系管理员！');
+            Toast({
+                message: '系统错误，请联系管理员！',
+                position: 'top',
+            });
         }
     },
 };
