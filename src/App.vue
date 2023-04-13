81dd95f46cf846a85a4a6695c8550624ef6fe55f
CN\jianglinghao <jianglinghao@corp.netease.com>
2023-04-13 17:38:34 +0800

    feat: 支持 制品 数帆登录互通

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,27 @@
     <div>
         <s-freesass-banner v-if="isFreeSass"></s-freesass-banner>
         <router-view></router-view>
-        <s-freesass-login ref="freeSassLogin"></s-freesass-login>
+         <s-freesass-login @afterShufanLogin="afterShufanLogin" ref="freeSassLogin"></s-freesass-login>
+        <s-freesass-transfer v-if="isFreeSass&&loginFinished" ref="freesassTransfer"></s-freesass-transfer>
     </div>
 </template>
 
 <script>
 import SFreesassLogin from '@/components/s-freesass-login.vue';
 import SFreesassBanner from '@/components/s-freesass-banner.vue';
+import SFreesassTransfer from '@/components/s-freesass-transfer';
 import auth from '@/apis/auth';
-
+const serviceMap = {
+    // generateSfToken: 'http://sfsso.community1.lcap.qz.163yun.com/api/generateSfToken',
+    checkSfToken: 'http://sfsso.community1.lcap.qz.163yun.com/api/checkSfToken',
+};
 export default {
-    components: { SFreesassLogin, SFreesassBanner },
+    components: { SFreesassLogin, SFreesassBanner,SFreesassTransfer },
+    data() {
+        return {
+            loginFinished: false,
+        };
+    },
     computed: {
         isSharePage() {
             const neteaseStrList = 'lcap.qz.163yun'.split('.');
@@ -25,14 +35,29 @@ export default {
     async mounted() {
         if (this.isSharePage && +window.appInfo?.tenantType === 1) {
             try {
-                const res = await auth.CheckExtendToken({ config: {
-                    noErrorTip: true,
-                } });
+                 // 校验接口
+                const res = await fetch(serviceMap.checkSfToken, {
+                    method: 'POST',
+                    mode: 'cors',
+                    credentials: 'include',
+                });
+                const data = await res.json();
+                // data.Code  === 200
+                if (data?.Data === true) {
+                    // 制品有sf_token 什么都不做
+                } else {
+                    this.$refs.freeSassLogin.open();
+                }
             } catch (error) {
                 console.error('CheckExtendToken: ', error);
                 this.$refs.freeSassLogin.open();
             }
         }
+    },
+     methods: {
+        afterShufanLogin() {
+            this.loginFinished = true;
+        },
     },
 };
 
