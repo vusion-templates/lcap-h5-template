6c21d240b0dc1d34955fc27eafa8c776f833f4a0
shenpengyun <shenpengyun@corp.netease.com>
2023-05-26 14:48:52 +0800

    Revert "feat：同步pc-template下载功能重命名"
    This reverts commit 4f90e1f0d9498122beceab87f79064665e23b86e.

--- src/utils/create/index.js
+++ src/utils/create/index.js
@@ -22,7 +22,7 @@ const formatContentType = function (contentType, data) {
  * 支持 query 参数
  */
 function download(url) {
-    const { path, method, body = {}, headers = {}, query = {}, timeout } = url;
+    const { path, method, body = {}, headers = {}, query = {} } = url;
 
     return axios({
         url: path,
@@ -30,21 +30,10 @@ function download(url) {
         params: query,
         data: formatContentType(headers['Content-Type'], body),
         responseType: 'blob',
-        timeout,
     }).then((res) => {
         // 包含 content-disposition， 从中解析名字，不包含 content-disposition 的获取请求地址的后缀
-        let effectiveFileName = res.request.getAllResponseHeaders().includes('content-disposition') ? getFilenameFromContentDispositionHeader(res.request.getResponseHeader('content-disposition')) : res.request.responseURL.split('/').pop();
-        effectiveFileName = decodeURIComponent(effectiveFileName);
+        const effectiveFileName = res.request.getAllResponseHeaders().includes('content-disposition') ? getFilenameFromContentDispositionHeader(res.request.getResponseHeader('content-disposition')) : res.request.responseURL.split('/').pop();
         const { data, status, statusText } = res;
-        // 如果没有size长度
-        if (data && data.size === 0) {
-            return Promise.resolve({
-                data: {
-                    code: status,
-                    msg: statusText,
-                },
-            });
-        }
         const downloadUrl = window.URL.createObjectURL(new Blob([data]));
         const link = document.createElement('a');
         link.href = downloadUrl;
