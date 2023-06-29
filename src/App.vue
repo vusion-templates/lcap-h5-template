ccb805b04bc51a7b6229bad350c7210ffebb75de
lzy940610 <lizhenyu0610@gmail.com>
2023-06-29 21:36:26 +0800

    feat: 单测 done

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,7 @@
 import SFreesassLogin from '@/components/s-freesass-login.vue';
 import SFreesassBanner from '@/components/s-freesass-banner.vue';
 import SFreesassTransfer from '@/components/s-freesass-transfer';
-import storage from './utils/storage/localStorage';
-import isEmpty from 'lodash/isEmpty';
+import { localCacheVariableMixin } from '@/mixins/localCacheVariableMixin';
 
 const newDomain = location.host.split('.').includes('163');
 const serviceMap = {
@@ -20,70 +19,7 @@ const serviceMap = {
     checkSfTokenNew: `${location.protocol}//sfsso-community1.app.codewave.163.com/api/checkSfToken`,
 };
 
-const ACTION_LOCAL_CACHE_VARIABLE_TYPE = {
-    GET: 'get',
-    UPDATE: 'update',
-    UNDEFINED: 'undefined'
-};
 
-// 定义一个名为 visibilityMixin 的全局混入对象
-const localCacheVariableMixin = {
-    beforeMount() {
-        this.actionLocalCacheVariable(ACTION_LOCAL_CACHE_VARIABLE_TYPE.GET);
-    },
-    mounted() {
-        document.addEventListener("visibilitychange", this.handleVisibilityChange);
-    },
-    beforeDestroy() {
-        this.actionLocalCacheVariable(ACTION_LOCAL_CACHE_VARIABLE_TYPE.UPDATE);
-        document.removeEventListener("visibilitychange", this.handleVisibilityChange);
-    },
-    methods: {
-        handleVisibilityChange() {
-            if (document.hidden && typeof this.actionLocalCacheVariable === 'function') {
-                this.actionLocalCacheVariable(ACTION_LOCAL_CACHE_VARIABLE_TYPE.UPDATE);
-            }
-        },
-        actionLocalCacheVariable(type = ACTION_LOCAL_CACHE_VARIABLE_TYPE.UNDEFINED) {
-            const localCacheVariableSet = this.$localCacheVariableSet;
-            const { frontendVariables } = this.$global;
-
-
-            for (const localCacheVariableKey of localCacheVariableSet) {
-
-                switch (type) {
-                    // 从 localCache 中获取数据
-                    case ACTION_LOCAL_CACHE_VARIABLE_TYPE.GET:
-                        const localCacheValue = storage.get(localCacheVariableKey, true);
-                        // 若存在 localCacheValue 则同步到 frontendVariables
-                        if (localCacheValue) {
-                            frontendVariables[localCacheVariableKey] = localCacheValue;
-                        }
-
-                        break;
-                    // 将 frontendVariables 中的数据同步到 localCache 触发时机 应用销毁前 & 应用切换到后台
-                    case ACTION_LOCAL_CACHE_VARIABLE_TYPE.UPDATE:
-                        const currentValue = frontendVariables[localCacheVariableKey];
-
-                        // 只同步写入非空值 避免 local 过多冗余数据
-                        if (isEmpty(currentValue)) {
-                            storage.remove(localCacheVariableKey);
-                        } else {
-                            storage.set(localCacheVariableKey, currentValue, true);
-                        }
-
-                        break;
-
-                    default:
-                        console.warn('actionLocalCacheVariable: type is undefined', type);
-                        break;
-                }
-
-            }
-        },
-
-    }
-};
 
 export default {
     mixins: [localCacheVariableMixin],
