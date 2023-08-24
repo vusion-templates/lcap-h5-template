4f78d2d37be08a62b564838ca0fec75178d5c917
zhongzichen <zhongzichen@corp.netease.com>
2023-08-24 15:43:58 +0800

    fix: 流程api接口补充

--- src/plugins/router/processService.js
+++ src/plugins/router/processService.js
@@ -45,3 +45,126 @@ export default {
         return res;
     },
 };
+
+export const porcessPorts = {
+    async getProcessDefinitionList(query) {
+        const res = await processInitService().getProcessDefinitionList({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getProcessDefinition(query) {
+        const res = await processInitService().getProcessDefinition({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getProcessInstanceList(query) {
+        const res = await processInitService().getProcessInstanceList({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getProcessInstance(query) {
+        const res = await processInitService().getProcessInstance({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getTaskDefinitionList(query) {
+        const res = await processInitService().getTaskDefinitionList({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getTaskDefinition(query) {
+        const res = await processInitService().getTaskDefinition({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getTaskInstanceList(query) {
+        const res = await processInitService().getTaskInstanceList({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getTaskInstance(query) {
+        const res = await processInitService().getTaskInstance({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async claimTaskInstance(query) {
+        const res = await processInitService().claimTaskInstance({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async unclaimTaskInstance(query) {
+        const res = await processInitService().unclaimTaskInstance({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getTaskDestinationUrl(query) {
+        const res = await processInitService().getTaskDestinationUrl({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async transferTaskInstance(query) {
+        const res = await processInitService().transferTaskInstance({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async withdrawProcessInstance(query) {
+        const res = await processInitService().withdrawProcessInstance({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async endProcessInstance(query) {
+        const res = await processInitService().endProcessInstance({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+    async getRejectableTaskDefinitionList(query) {
+        const res = await processInitService().getRejectableTaskDefinitionList({
+            body: {
+                ...query,
+            },
+        });
+        return res;
+    },
+};
